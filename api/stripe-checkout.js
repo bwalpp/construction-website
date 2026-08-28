90a92ccb6554a4a4da6c117b851dd03bc014c53b
bwalpp <vincentleiztairbrazil3@gmail.com>
2026-08-28 18:04:27 +0800

    Update stripe-checkout.js

--- api/stripe-checkout.js
+++ api/stripe-checkout.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.stripe_secret_key;
 
 module.exports = async (req, res) => {
   const { name, price, quantity } = req.body;
